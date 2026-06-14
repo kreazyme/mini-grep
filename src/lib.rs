2bd7d25b1002dabda435b1d6c8537cfc4f4361bf
Trần Đức Thông <thong.tran@enouvo.com>
2026-06-14 22:17:16 +0700

    revamped

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,56 @@
 use std::fs;
 
-pub fn read_file(path: String) -> String {
-    let file_content = fs::read_to_string(path);
-    return match file_content {
-        Ok(text) => text,
-        Err(err) => {
-            println!("{}", err);
-            return String::from("");
-        }
-    };
+pub fn run(config: Config) {
+    let filedata = search(config.query, read_file(config.file_name));
+    print!("result: {}", filedata.len());
+    for line in filedata {
+        println!("{}", line);
+    }
 }
 
-pub fn search_case_insensitive(content: String, keyword: String) -> Vec<String> {
-    let keyword = keyword.to_lowercase();
-    return content
+pub struct Config {
+    pub file_name: String,
+    pub is_sensitive: bool,
+    pub query: String,
+}
+
+impl Config {
+    pub fn new(args: Vec<String>) -> Result<Config, &'static str> {
+        let mut data = args.iter();
+        data.next();
+        let file_name = match data.next() {
+            Some(val) => val,
+            None => return Err("Please provide a file path as an argument."),
+        };
+
+        let is_sensitive = match data.next() {
+            Some(val) => val == "true",
+            None => return Err("Please specify if the search is sensitive."),
+        };
+
+        let query = match data.next() {
+            Some(val) => val,
+            None => return Err("Please provide a query string."),
+        };
+
+        Ok(Config {
+            file_name: file_name.clone(),
+            is_sensitive,
+            query: query.clone(),
+        })
+    }
+}
+
+fn read_file(path: String) -> String {
+    print!("Reading file: {}", path);
+    let content = fs::read_to_string(path);
+    return content.unwrap();
+}
+
+fn search(keyword: String, file_content: String) -> Vec<String> {
+    file_content
         .lines()
-        .filter(|line| line.to_lowercase().contains(&keyword))
+        .filter(|line: &&str| line.contains(&keyword))
         .map(String::from)
-        .collect();
+        .collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
 use std::env;
 
-use mini_grep::{read_file, search_case_insensitive};
+use mini_grep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    print!("{:?}", args);
-    if args.len() < 2 {
-        println!("Please provide a file path as an argument.");
-        return;
-    }
-    let mut file_content = read_file(args[1].clone());
-
-    if !args[2].is_empty() {
-        file_content = search_case_insensitive(file_content, args[2].clone()).join("\n");
-    }
-    println!("{}", file_content);
+    let config = Config::new(args).unwrap();
+    mini_grep::run(config);
 }
